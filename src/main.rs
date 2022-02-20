db0c559c3b35fcb9ab9dc5aef7a34bafc090372d
Billy Hadlow <me@billyhadlow.com>
2022-02-20 15:06:58 +0000

    project setup

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+
+fn align(seq1: &str, seq2: &str) -> String
+{
+    String::new()
+}
+
+fn main()
+{
+    let seq1: &str = "ACGGCTC";
+    let seq2: &str = "ATGGCCTC";
+
+    let map = align(seq1, seq2);
+
+    println!("Hello, world!");
+}
